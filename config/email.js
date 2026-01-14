f50ab206cd57d37c88194919c77b9caacc03d76e
unknown <ortizmoncada2006@gmail.com>
2026-01-14 15:36:41 -0500

    2525

--- config/email.js
+++ config/email.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 2525,
   secure: false, // true para puerto 465, false para puerto 587
   auth: {
     user: process.env.EMAIL_USER,
